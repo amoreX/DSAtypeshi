8032383fe3c552830c1d31b4ef7a8083132546e5
Md Nihal Rahman <rahmannihal72@gmail.com>
2025-01-14 16:16:06 +0530

    trie

---
+++ implementing-trie.py
@@ -0,0 +1,39 @@
+class Node:
+    def __init__(self,val):
+        self.val= val
+        self.nodes={}
+        self.is_end=False
+
+class Trie:
+    def __init__(self):
+        self.root=Node()
+    
+    def insert(self,word):
+        node=self.root
+        for char in word:
+            if char not in node.nodes:
+                tempnode=Node(char)
+                node.nodes[char]=tempnode
+            node=node.nodes[char]
+        node.is_end=True
+
+    def search(self,word):
+        node=self.root
+        for char in word:
+            if char not in node.nodes:
+                return False
+            node=node.nodes[char]
+        return node.is_end
+
+    def startsWith(self,prefix):
+        node=self.root
+        for char in prefix:
+            if char not in node.nodes:
+                return False
+            node=node.nodes[char]
+        return True
+
+trie=Trie()
+trie.insert("apple")
+print(trie.search("apple"))
+print(trie.startsWith("app"))
